c03a912a4ae48e01746d39281c1cf0b1bb5e0230
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-11-30 20:08:56 -0500

    fixing repo

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -19,7 +19,7 @@ export default function About() {
           My name is Kiyona Bentley. I am 25 years old and was born and raised in Philadelphia, PA. 
           I have a bachelor's degree in Psychology and a Master's degree in Buisness Administration. 
           I have just started my journey into the world of full-stack web development and I am always excited to learn more. 
-          I am currently enrolled in the University of Pennsylvania Lps coding bootcamp where I am learning to develop and hone my web development skills.
+          Recently, I received a certificate from University of Pennsylvania Lps coding bootcamp where I learning to develop and hone my web development skills. When I am not cooding I enjoy watching movies and playing all different kinds of video games.
           </p>
         </div>
       </div>
